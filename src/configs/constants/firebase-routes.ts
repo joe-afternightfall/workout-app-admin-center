d7039aac2355a62b1fc41893fff736f2a612c7a8
joe-afternightfall <joe@afternightfall.com>
2021-10-02 15:19:48 -0500

    updating routes

--- src/configs/constants/firebase-routes.ts
+++ src/configs/constants/firebase-routes.ts
@@ -3,6 +3,7 @@ const BASE_CONFIG_ROUTE = '/workout-configurations';
 export const CIRCUIT_TYPES_ROUTE = `${BASE_CONFIG_ROUTE}/circuit-types`;
 export const EXERCISE_TYPES_ROUTE = `${BASE_CONFIG_ROUTE}/exercise-types`;
 export const CATEGORY_TYPES_ROUTE = `${BASE_CONFIG_ROUTE}/category-types`;
+export const EXERCISES_DB_ROUTE = `${BASE_CONFIG_ROUTE}/exercises`;
 export const WORKOUTS_ROUTE = '/workouts';
 export const USER_PROFILES_ROUTE = '/user-profiles';
 export const CIRCUIT_TEMPLATES_ROUTE = `${BASE_CONFIG_ROUTE}/circuit-templates`;
