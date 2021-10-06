0ff364dbfa64168ce8f42ac5905fdd5bcc98d8ad
joe-afternightfall <joe@afternightfall.com>
2021-10-05 19:40:53 -0500

    exercise card setup

--- src/components/top-level-components/routine-builder/form-view/routine-info/components/sections/ExerciseCard.tsx
+++ src/components/top-level-components/routine-builder/form-view/routine-info/components/sections/ExerciseCard.tsx
@@ -3,22 +3,13 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import {
-  Grid,
-  List,
-  ListItemText,
-  ListItem,
-  TextField,
-  Divider,
-} from '@material-ui/core';
+import { ListItemText, ListItem, TextField, Divider } from '@material-ui/core';
 import { ExerciseVO, Segment } from 'workout-app-common-core';
 import { State } from '../../../../../../../configs/redux/store';
-import {
-  addSetToExercise,
-  updateSegmentExercise,
-} from '../../../../../../../creators/routine-builder/builder';
+import { updateSegmentExercise } from '../../../../../../../creators/routine-builder/builder';
 import SetIncrementer from '../inputs/SetIncrementer';
 import RestBetweenDropdown from '../inputs/RestBetweenDropdown';
+import BaseListItem from '../BaseListItem';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,7 +20,6 @@ const useStyles = makeStyles((theme: Theme) =>
 const ExerciseCard = ({
   exercises,
   segment,
-  addSetHandler,
   selectExerciseHandler,
 }: ExerciseCardProps & PassedInProps): JSX.Element => {
   const classes = useStyles();
@@ -47,73 +37,53 @@ const ExerciseCard = ({
   });
 
   return (
-    <Grid item xs={12} container>
-      <Grid item xs={12}>
-        <Autocomplete
-          fullWidth
-          // value={defaultValue}
-          id={`${segment.id}-exercise-menu`}
-          options={exercises}
-          getOptionLabel={(option) => option.name}
-          renderInput={(params) => (
-            <TextField {...params} label={'Exercise'} variant={'outlined'} />
-          )}
-          onChange={(e: ChangeEvent<Record<string, never>>, newValue) => {
-            newValue && selectExerciseHandler(newValue.id);
-          }}
-          getOptionSelected={(option, value) => option.id === value.id}
-        />
-      </Grid>
+    <>
+      <BaseListItem
+        title={'Exercise'}
+        component={
+          <Autocomplete
+            fullWidth
+            // value={defaultValue}
+            id={`${segment.id}-exercise-menu`}
+            options={exercises}
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => (
+              <TextField {...params} label={'Exercise'} variant={'outlined'} />
+            )}
+            onChange={(e: ChangeEvent<Record<string, never>>, newValue) => {
+              newValue && selectExerciseHandler(newValue.id);
+            }}
+            getOptionSelected={(option, value) => option.id === value.id}
+          />
+        }
+      />
       {selectedExercise &&
         segment.exercises.map((workoutExercise) => {
           if (workoutExercise.exerciseId === selectedExerciseId) {
             return (
-              <Grid item xs={12}>
-                <List>
-                  <ListItem>
-                    <ListItemText
-                      disableTypography
-                      primary={<SetIncrementer />}
-                    />
-                  </ListItem>
-
-                  <ListItem>
-                    <ListItemText
-                      disableTypography
-                      primary={<Divider variant={'middle'} />}
-                    />
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText
-                      disableTypography
-                      primary={
-                        <RestBetweenDropdown
-                          value={String(segment.secondsRestBetweenSets)}
-                          segmentId={segment.id}
-                          type={'Sets'}
-                        />
-                      }
-                    />
-                  </ListItem>
-
-                  <ListItem>
-                    <ListItemText
-                      disableTypography
-                      primary={
-                        <RestBetweenDropdown
-                          value={String(segment.secondsRestBetweenNextSegment)}
-                          segmentId={segment.id}
-                          type={'Segment'}
-                        />
-                      }
-                    />
-                  </ListItem>
-                </List>
-              </Grid>
+              <>
+                <SetIncrementer />
+                <ListItem>
+                  <ListItemText
+                    disableTypography
+                    primary={<Divider variant={'middle'} />}
+                  />
+                </ListItem>
+                <RestBetweenDropdown
+                  value={String(segment.secondsRestBetweenSets)}
+                  segmentId={segment.id}
+                  type={'Sets'}
+                />
+                <RestBetweenDropdown
+                  value={String(segment.secondsRestBetweenNextSegment)}
+                  segmentId={segment.id}
+                  type={'Segment'}
+                />
+              </>
             );
           }
         })}
-    </Grid>
+    </>
   );
 };
 
@@ -123,7 +93,6 @@ interface PassedInProps {
 
 interface ExerciseCardProps {
   exercises: ExerciseVO[];
-  addSetHandler: (workoutExerciseId: string) => void;
   selectExerciseHandler: (exerciseId: string) => void;
 }
 
@@ -140,9 +109,6 @@ const mapDispatchToProps = (
   ({
     selectExerciseHandler: (exerciseId: string) => {
       dispatch(updateSegmentExercise(ownProps.segment.id, exerciseId));
-    },
-    addSetHandler: (exerciseId: string) => {
-      dispatch(addSetToExercise(exerciseId));
     },
   } as unknown as ExerciseCardProps);
 
