4444cd63f5eac288e18c2d32aa9ebf921e62233b
joe-yarbrough <joe@afternightfall.com>
2021-10-14 20:40:03 -0500

    setup completed exercies

--- src/components/top-level-components/routine-builder-screen/routine-info/components/exercise-segment/completed-segment/components/CompletedExercises.tsx
+++ src/components/top-level-components/routine-builder-screen/routine-info/components/exercise-segment/completed-segment/components/CompletedExercises.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link as LinkIcon } from '@material-ui/icons';
 import { Grid, Typography, Divider } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { State } from '../../../../../../../../configs/redux/store';
 import { ExerciseVO, getExerciseName, Segment } from 'workout-app-common-core';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     iconWrapper: {
       height: '100%',
+    },
+    icon: {
+      color: theme.palette.text.secondary,
     },
   })
 );
 
 const CompletedExercises = ({
-  icon,
+  linkIconSize,
   exercises,
   segment,
   setType,
+  hideNumbers,
 }: CompletedExercisesProps & PassedInProps): JSX.Element => {
   const classes = useStyles();
 
+  let firstElementTitle = '';
+  let secondElementTitle = '';
+
+  if (setType === 'superset') {
+    firstElementTitle =
+      segment.exercises[0] &&
+      getExerciseName(exercises, segment.exercises[0].exerciseId);
+    secondElementTitle =
+      segment.exercises[1] &&
+      getExerciseName(exercises, segment.exercises[1].exerciseId);
+  } else if (setType === 'straight-set') {
+    firstElementTitle =
+      segment.exercises[0] &&
+      getExerciseName(exercises, segment.exercises[0].exerciseId);
+  }
+
   return (
     <>
-      {setType === 'super' && (
+      {setType === 'superset' && (
         <Grid container>
           <Grid item xs={12}>
             <Typography variant={'h6'} color={'textPrimary'}>
-              {`1. ${
-                segment.exercises[0] &&
-                getExerciseName(exercises, segment.exercises[0].exerciseId)
-              }`}
+              {hideNumbers ? firstElementTitle : `1. ${firstElementTitle}`}
             </Typography>
           </Grid>
           <Grid item xs={12}>
@@ -44,27 +62,21 @@ const CompletedExercises = ({
                 <Divider variant={'fullWidth'} />
               </Grid>
               <Grid item xs={2}>
-                {icon}
+                <LinkIcon className={classes.icon} fontSize={linkIconSize} />
               </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12}>
             <Typography variant={'h6'} color={'textPrimary'}>
-              {`2. ${
-                segment.exercises[1] &&
-                getExerciseName(exercises, segment.exercises[1].exerciseId)
-              }`}
+              {hideNumbers ? secondElementTitle : `2. ${secondElementTitle}`}
             </Typography>
           </Grid>
         </Grid>
       )}
-      {setType === 'straight' && (
+      {setType === 'straight-set' && (
         <Grid>
           <Typography variant={'h6'} color={'textPrimary'}>
-            {`1. ${
-              segment.exercises[0] &&
-              getExerciseName(exercises, segment.exercises[0].exerciseId)
-            }`}
+            {hideNumbers ? firstElementTitle : `1. ${firstElementTitle}`}
           </Typography>
         </Grid>
       )}
@@ -73,9 +85,10 @@ const CompletedExercises = ({
 };
 
 interface PassedInProps {
+  hideNumbers?: boolean;
   segment: Segment;
-  icon: JSX.Element;
-  setType: 'super' | 'straight' | null;
+  linkIconSize: 'inherit' | 'default' | 'small' | 'large';
+  setType: 'superset' | 'straight-set' | 'circuit-set' | null;
 }
 
 interface CompletedExercisesProps {
