bcdaadf99a8d13c3220f75b298e5be098dc38759
joe-yarbrough <joe@afternightfall.com>
2021-10-16 09:01:33 -0500

    setup list item card for delete

--- src/components/timer-dialog/input-settings/components/TimerListItemCard.tsx
+++ src/components/timer-dialog/input-settings/components/TimerListItemCard.tsx
@@ -1,103 +1,52 @@
 import React from 'react';
 import {
-  List,
   Card,
-  Grid,
-  Divider,
   ListItem,
   Typography,
   IconButton,
   ListItemText,
-  ListItemSecondaryAction,
   ListItemIcon,
-  Fade,
+  ListItemSecondaryAction,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { WorkoutTimer } from 'workout-app-common-core';
+import { NewWorkoutTimer } from '../InputSettings';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
-import EditingSegmentContainer from '../../../top-level-components/routine-builder-screen/routine-info/components/exercise-segment/editing-segment/EditingSegmentContainer';
-import CompletedSegmentCard from '../../../top-level-components/routine-builder-screen/routine-info/components/exercise-segment/completed-segment/CompletedSegmentCard';
 
 const useStyles = makeStyles(() =>
   createStyles({
-    contentWrapper: {
-      minHeight: '30vh',
-      height: '100%',
-      textAlign: 'center',
-      paddingBottom: 16,
-    },
-    dialogTitle: {
-      padding: '16px 0',
-    },
-    dragIndicator: {
-      transform: 'rotate(90deg)',
-    },
-    indicatorContainer: {
-      '&:hover': {
-        cursor: 'move',
-      },
-      opacity: 0.5,
-      // marginBottom: -24,
-      minHeight: 20,
-      width: '100%',
+    root: {
+      paddingTop: 4,
     },
   })
 );
 
 export default function TimerListItemCard({
   timer,
+  deleteHandler,
 }: TimerListItemCardProps): JSX.Element {
   const classes = useStyles();
-  const [displayDragHandler, setDisplayDragHandler] = React.useState(false);
 
   return (
-    <div
-      onMouseOver={() => {
-        setDisplayDragHandler(true);
-      }}
-      onMouseLeave={() => {
-        setDisplayDragHandler(false);
-      }}
-    >
-      {/*<Grid*/}
-      {/*  item*/}
-      {/*  xs={12}*/}
-      {/*  container*/}
-      {/*  justify={'center'}*/}
-      {/*  className={clsx(*/}
-      {/*    'timer-list-item-drag-handle',*/}
-      {/*    classes.indicatorContainer*/}
-      {/*  )}*/}
-      {/*>*/}
-      {/*  <Fade in={displayDragHandler}>*/}
-      {/*    <DragIndicatorIcon*/}
-      {/*      className={clsx(classes.dragIndicator)}*/}
-      {/*      fontSize={'small'}*/}
-      {/*    />*/}
-      {/*  </Fade>*/}
-      {/*</Grid>*/}
-      <Card style={{ paddingTop: 4 }}>
-        <ListItem>
-          <ListItemIcon>
-            <Typography>{`${timer.order}.`}</Typography>
-          </ListItemIcon>
-          <ListItemText
-            primary={`${timer.seconds} seconds`}
-            secondary={timer.stepperTitle}
-          />
-          <ListItemSecondaryAction>
-            <IconButton>
-              <CloseIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-      </Card>
-    </div>
+    <Card className={classes.root}>
+      <ListItem>
+        <ListItemIcon>
+          <Typography>{`${timer.order}.`}</Typography>
+        </ListItemIcon>
+        <ListItemText
+          primary={`${timer.seconds} seconds`}
+          secondary={timer.stepperTitle}
+        />
+        <ListItemSecondaryAction>
+          <IconButton onClick={() => deleteHandler(timer)}>
+            <CloseIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </Card>
   );
 }
 
 interface TimerListItemCardProps {
-  timer: WorkoutTimer;
+  timer: NewWorkoutTimer;
+  deleteHandler: (timer: NewWorkoutTimer) => void;
 }
