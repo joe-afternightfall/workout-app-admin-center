7f9c172ff33616758b5d644e29f123d70cc563d3
joe-yarbrough <joe@afternightfall.com>
2021-10-20 13:28:01 -0500

    screen cleanup

--- src/components/top-level-components/exercises-screen/ExercisesScreen.tsx
+++ src/components/top-level-components/exercises-screen/ExercisesScreen.tsx
@@ -8,78 +8,23 @@ import React, { Component } from 'react';
 import { Styles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 import ExerciseListTable from './ExerciseListTable';
-import SwipeableViews from 'react-swipeable-views';
-import FormView from './form-view/FormView';
-import { ExerciseVO } from 'workout-app-common-core';
+import FormDialog from './form-dialog/ExerciseFormDialog';
 
 const styles: Styles<Theme, StyledComponentProps> = () => ({});
 
-interface LocalState {
-  activeTab: number;
-  newExercise: boolean;
-  exercise: ExerciseVO | undefined;
-}
-
-class ExercisesScreen extends Component<ExercisesScreenProps, LocalState> {
-  state = {
-    activeTab: 0,
-    newExercise: false,
-    exercise: undefined,
-  };
-
+class ExercisesScreen extends Component {
   render(): JSX.Element {
-    const handleChange = (tab: number, newExercise: boolean) => {
-      this.setState({
-        activeTab: tab,
-        newExercise: newExercise,
-      });
-    };
-
-    const handleEditClick = (exercise: ExerciseVO) => {
-      this.setState({
-        activeTab: 1,
-        newExercise: false,
-        exercise: exercise,
-      });
-    };
-
-    const backToListHandler = () => {
-      this.setState({
-        activeTab: 0,
-        newExercise: false,
-      });
-    };
-
     return (
       <Grid container>
+        <FormDialog />
         <Grid item xs={12}>
-          <SwipeableViews
-            index={this.state.activeTab}
-            // onChangeIndex={handleChange}
-            containerStyle={{
-              transition: 'transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s',
-            }}
-          >
-            <ExerciseListTable
-              actionClickHandler={(newExercise: boolean) => {
-                handleChange(1, newExercise);
-              }}
-              editClickHandler={handleEditClick}
-            />
-            <FormView
-              newExercise={this.state.newExercise}
-              successCallback={backToListHandler}
-              exercise={this.state.exercise}
-            />
-          </SwipeableViews>
+          <ExerciseListTable />
         </Grid>
       </Grid>
     );
   }
 }
 
-export interface ExercisesScreenProps extends WithStyles<typeof styles> {
-  DELETE_ME?: undefined;
-}
+export type ExercisesScreenProps = WithStyles<typeof styles>;
 
 export default withStyles(styles, { withTheme: true })(ExercisesScreen);
