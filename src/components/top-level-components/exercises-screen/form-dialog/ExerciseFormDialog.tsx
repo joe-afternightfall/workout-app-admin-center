44c71e78f0217c554437d829e317c363a3f9e1fa
joe-yarbrough <joe@afternightfall.com>
2021-10-20 13:28:19 -0500

    creating dialog

---
+++ src/components/top-level-components/exercises-screen/form-dialog/ExerciseFormDialog.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Dialog } from '@material-ui/core';
+import { AnyAction, Dispatch } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import SaveIcon from '@material-ui/icons/Save';
+import { Button, Grid } from '@material-ui/core';
+import { State } from '../../../../configs/redux/store';
+import ExerciseInfoContent from './ExerciseInfoContent';
+import BaseDialogContent from '../../../shared/BaseDialogContent';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { saveExercise } from '../../../../services/workout-configurations/exercises';
+import { closeExerciseFormDialog } from '../../../../creators/exercise-form/exercise-form';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    deleteButton: {
+      color: theme.palette.error.main,
+    },
+    content: {
+      minHeight: '20vh',
+    },
+  })
+);
+
+const ExerciseFormDialog = (props: ExerciseFormDialogProps) => {
+  const { open, isNewExercise, closeHandler } = props;
+  const classes = useStyles();
+  return (
+    <Dialog open={open} onClose={closeHandler}>
+      <BaseDialogContent
+        title={isNewExercise ? 'New Exercise' : 'Edit Exercise'}
+        closeClickHandler={closeHandler}
+        dialogContent={
+          <Grid
+            container
+            alignItems={'center'}
+            justify={'center'}
+            className={classes.content}
+          >
+            <Grid item>
+              <ExerciseInfoContent />
+            </Grid>
+          </Grid>
+        }
+        dialogActions={
+          <Grid item container justify={'flex-end'}>
+            <Button
+              color={'primary'}
+              startIcon={<SaveIcon />}
+              onClick={() => {
+                props.saveExerciseClickHandler(closeHandler);
+              }}
+            >
+              {'Save'}
+            </Button>
+          </Grid>
+        }
+      />
+    </Dialog>
+  );
+};
+
+interface ExerciseFormDialogProps {
+  open: boolean;
+  isNewExercise: boolean;
+  closeHandler: () => void;
+  saveExerciseClickHandler: (successCallback: () => void) => void;
+}
+
+const mapStateToProps = (state: State): ExerciseFormDialogProps => {
+  return {
+    isNewExercise: state.exerciseFormState.newExerciseForm,
+    open: state.exerciseFormState.openExerciseFormDialog,
+  } as unknown as ExerciseFormDialogProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ExerciseFormDialogProps =>
+  ({
+    saveExerciseClickHandler: (successCallback: () => void) => {
+      (dispatch as ThunkDispatch<State, void, AnyAction>)(
+        saveExercise(successCallback)
+      );
+    },
+    closeHandler: () => {
+      dispatch(closeExerciseFormDialog());
+    },
+  } as unknown as ExerciseFormDialogProps);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExerciseFormDialog);
