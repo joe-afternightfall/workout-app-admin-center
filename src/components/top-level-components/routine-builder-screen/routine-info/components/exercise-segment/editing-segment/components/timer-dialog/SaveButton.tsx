bb859cbdea2cd9c9c7c2f6992e0ad64d92cbc5fb
joe-yarbrough <joe@afternightfall.com>
2021-10-19 21:30:55 -0500

    setup save button

---
+++ src/components/top-level-components/routine-builder-screen/routine-info/components/exercise-segment/editing-segment/components/timer-dialog/SaveButton.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { Button } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { addTimerToWorkoutExercise } from '../../../../../../../../../creators/routine-builder/builder';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      marginLeft: 'auto',
+    },
+  })
+);
+
+const SaveButton = (props: SaveButtonProps & PassedInProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <Button className={classes.root} onClick={props.saveHandler}>
+      {'Save'}
+    </Button>
+  );
+};
+
+export interface SaveButtonProps {
+  saveHandler: () => void;
+}
+
+export interface PassedInProps {
+  workoutExerciseId: string;
+  closeHandler: () => void;
+}
+
+const mapDispatchToProps = (
+  dispatch: Dispatch,
+  ownProps: PassedInProps
+): SaveButtonProps =>
+  ({
+    saveHandler: () => {
+      dispatch(addTimerToWorkoutExercise(ownProps.workoutExerciseId));
+      ownProps.closeHandler();
+    },
+  } as unknown as SaveButtonProps);
+
+export default connect(null, mapDispatchToProps)(SaveButton);
