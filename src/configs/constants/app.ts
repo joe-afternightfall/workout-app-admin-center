c9d296a70542b2468dd61bf17fe16e8dd9b901e9
joe-yarbrough <joe@afternightfall.com>
2021-10-31 23:58:09 -0500

    adding path

--- src/configs/constants/app.ts
+++ src/configs/constants/app.ts
@@ -9,3 +9,4 @@ export const EXERCISES_SCREEN_PATH = '/exercises';
 export const ROUTINE_BUILDER_SCREEN_PATH = '/routine-builder';
 export const ROUTINE_TEMPLATES_SCREEN_PATH = '/routine-templates';
 export const EXERCISE_TYPES_SCREEN_PATH = '/configs/exercise-types';
+export const APP_CONFIGS_SCREEN_PATH = '/app-configs';
