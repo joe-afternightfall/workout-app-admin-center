0a29ab221a6a45ba249d6b9c76777c240924253d
joe-yarbrough <joe@afternightfall.com>
2021-11-02 10:29:36 -0500

    param type button group fix

--- src/components/top-level-components/exercises-screen/form-dialog/info-components/ParamTypeButtonGroup.tsx
+++ src/components/top-level-components/exercises-screen/form-dialog/info-components/ParamTypeButtonGroup.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 import { Grid, Typography } from '@material-ui/core';
-import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import { ParameterTypeVO } from 'workout-app-common-core';
+import { State } from '../../../../../configs/redux/store';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { selectExerciseParamType } from '../../../../../creators/exercise-form/exercise-form';
 
-export default function ParamTypeButtonGroup({
+const ParamTypeButtonGroup = ({
   changeHandler,
   parameterTypes,
   selectedParamType,
-}: ParamTypeButtonGroupProps): JSX.Element {
+}: ParamTypeButtonGroupProps): JSX.Element => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -17,6 +21,7 @@ export default function ParamTypeButtonGroup({
       </Grid>
       <Grid item xs={12}>
         <ToggleButtonGroup
+          style={{ width: '100%' }}
           exclusive
           value={selectedParamType}
           onChange={(
@@ -27,7 +32,7 @@ export default function ParamTypeButtonGroup({
           }}
         >
           {parameterTypes.map((type: ParameterTypeVO, index: number) => (
-            <ToggleButton value={type} key={index}>
+            <ToggleButton value={type} key={index} style={{ width: '25%' }}>
               {type.name}
             </ToggleButton>
           ))}
@@ -42,10 +47,36 @@ export default function ParamTypeButtonGroup({
       )}
     </Grid>
   );
-}
+};
 
 interface ParamTypeButtonGroupProps {
   changeHandler: (paramType: ParameterTypeVO) => void;
   selectedParamType: ParameterTypeVO | null;
   parameterTypes: ParameterTypeVO[];
 }
+
+const mapStateToProps = (state: State): ParamTypeButtonGroupProps => {
+  const exerciseForm = state.exerciseFormState.exerciseForm;
+
+  const parameterTypes =
+    state.applicationState.workoutConfigurations.parameterTypes;
+  const selectedParamType = parameterTypes.find(
+    (param) => param.id === exerciseForm.parameterTypeId
+  );
+  return {
+    selectedParamType: selectedParamType,
+    parameterTypes: parameterTypes,
+  } as unknown as ParamTypeButtonGroupProps;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): ParamTypeButtonGroupProps =>
+  ({
+    changeHandler: (paramType: ParameterTypeVO) => {
+      dispatch(selectExerciseParamType(paramType));
+    },
+  } as unknown as ParamTypeButtonGroupProps);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ParamTypeButtonGroup);
