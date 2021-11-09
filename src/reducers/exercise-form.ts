30955b9cdb472772d566fe1dafb16d23978002e1
joe-yarbrough <joe@afternightfall.com>
2021-11-09 12:53:34 -0600

    lint

--- src/reducers/exercise-form.ts
+++ src/reducers/exercise-form.ts
@@ -1,8 +1,5 @@
 import { ActionTypes, ApplicationActions } from '../creators/actions';
-import {
-  ExerciseVO,
-  PRIMARY_MUSCLE_TARGET_TYPE_ID,
-} from 'workout-app-common-core';
+import { ExerciseVO } from 'workout-app-common-core';
 import { v4 as uuidv4 } from 'uuid';
 import * as ramda from 'ramda';
 import { primaryMuscle, secondaryMuscle } from '../utils/muscle-target-builder';
